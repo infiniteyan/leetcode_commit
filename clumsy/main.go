ba53e74d5319f66000c324aadfef826ed316b4f0
infiniteyan <scorpio.yansw@gmail.com>
2020-03-01 23:18:26 +0800

    笨阶乘

--- clumsy/main.go
+++ clumsy/main.go
@@ -1,1 +1,51 @@
-package clumsy
+package main
+
+import "fmt"
+
+func clumsy(N int) int {
+	loops := N / 4
+	left := N % 4
+
+	var ret int
+	for i := 0; i < loops; i++ {
+		base := N - i * 4
+		var elementValue int
+		if i == 0 {
+			elementValue = base * (base - 1) / (base - 2) + (base - 3)
+			ret += elementValue
+		} else {
+			elementValue = base * (base - 1) / (base - 2) - (base - 3)
+			ret -= elementValue
+		}
+	}
+
+	if left > 0 {
+		if (loops > 0) {
+			switch left {
+			case 1:
+				ret -= left
+			case 2:
+				ret -= left * (left - 1)
+			case 3:
+				ret -= left * (left - 1) * (left - 2)
+			}
+		} else {
+			switch left {
+			case 1:
+				ret += left
+			case 2:
+				ret += left * (left - 1)
+			case 3:
+				ret += left * (left - 1) * (left - 2)
+			}
+		}
+	}
+
+	return ret
+}
+
+func main() {
+	fmt.Println(clumsy(1))
+	fmt.Println(clumsy(4))
+	fmt.Println(clumsy(10))
+}
